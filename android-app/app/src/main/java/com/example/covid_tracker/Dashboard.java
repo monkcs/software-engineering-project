1dea19fa0b7f055e8b5186b632e5dfc4fa22b78f
Calle <calle.samuelsson.14@gmail.com>
2021-09-07 13:00:35 +0200

    Fixi fixi

--- android-app/app/src/main/java/com/example/covid_tracker/Dashboard.java
+++ android-app/app/src/main/java/com/example/covid_tracker/Dashboard.java
@@ -2,13 +2,43 @@ package com.example.covid_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
+        RelativeLayout bokavaccin1 = (RelativeLayout) findViewById(R.id.bokavaccin1);
+        bokavaccin1.setOnClickListener(this);
+    }
+
+    public void openBokavaccin()
+    {
+        Intent intent = new Intent(this, Boka_vaccin.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+
+            case R.id.bokavaccin1:
+                System.out.println("button has been pressed");
+                openBokavaccin();
+                break;
+
+
+        }
     }
 }
