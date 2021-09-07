da11e4ea333eb35f214aa95d033c7de1b953b1eb
monkcs <charlie.habolin@citroner.blog>
2021-09-07 09:49:59 +0200

    Change git folder structure

---
+++ android-app/app/src/main/java/com/example/covid_tracker/Login.java
@@ -0,0 +1,14 @@
+package com.example.covid_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_login);
+    }
+}
