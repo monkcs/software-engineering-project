27f361d3584bd5e30f4075193e136dae262f392d
monkcs <charlie.habolin@citroner.blog>
2021-09-14 23:36:19 +0200

    Implement signup and login for users

--- android-app/app/src/main/java/com/example/covid_tracker/Login.java
+++ android-app/app/src/main/java/com/example/covid_tracker/Login.java
@@ -70,7 +70,7 @@ public class Login extends AppCompatActivity implements OnClickListener {
     }
 
     void login() {
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, WebRequest.urlbase + "authenticate-user.php", null,
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, WebRequest.urlbase + "user/information.php", null,
                 response -> {
                     WebRequest.username = user_email.getText().toString();
                     WebRequest.password = user_password.getText().toString();
@@ -91,6 +91,17 @@ public class Login extends AppCompatActivity implements OnClickListener {
         queue.add(request);
     }
 
+
+    public void signup() {
+        Intent intent = new Intent(this, Regristering.class);
+        startActivity(intent);
+    }
+
+    public void loginAdmin() {
+        Intent intent = new Intent(this, Administartorlogin.class);
+        startActivity(intent);
+    }
+
     public void onClick(View view) {
         switch (view.getId()) {
 
@@ -107,74 +118,6 @@ public class Login extends AppCompatActivity implements OnClickListener {
             case R.id.adminLogin:
                 loginAdmin();
                 break;
-        }
-    }
-
-    public void signup() {
-        Intent intent = new Intent(this, Regristering.class);
-        startActivity(intent);
-    }
-
-    public void loginAdmin() {
-        Intent intent = new Intent(this, Administartorlogin.class);
-        startActivity(intent);
-    }
-
-    class AttemptLogin extends AsyncTask<String, String, String> {
-        boolean failure = false;
-
-        protected void onPreExcecute() {
-            super.onPreExecute();
-            pDialog = new ProgressDialog(Login.this);
-            pDialog.setMessage("Attempting for login...");
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(true);
-            pDialog.show();
-        }
-
-        @Override
-        protected String doInBackground(String... args) {
-            int success;
-            String username = user_email.getText().toString();
-            String pass = user_password.getText().toString();
-
-            try {
-                List<NameValuePair> params = new ArrayList<>();
-                params.add(new BasicNameValuePair("username", username));
-                params.add(new BasicNameValuePair("password", pass));
-                Log.d("request!", "starting");
-
-                JSONObject json = jsonParser.makeHttpRequest(LOGIN_URL, "POST", params);
-
-                Log.d("Login attempt", json.toString());
-
-                success = json.getInt(TAG_SUC);
-                System.out.println(success);
-                if (success == 1) {
-                    Log.d("Successfully Login!", json.toString());
-                    //Lägg till när klassen skapats
-                    Intent intent = new Intent(Login.this, LoginDashboard.class);
-                    finish();
-                    startActivity(intent);
-                    return json.getString(TAG_MSG);
-                } else {
-                    return json.getString(TAG_MSG);
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        protected void onPostExecute(String message) {
-            if (pDialog != null && pDialog.isShowing()) {
-                pDialog.dismiss();
-            }
-
-            if (message != null) {
-                Toast.makeText(Login.this, message, Toast.LENGTH_LONG).show();
-            }
         }
     }
 }
