99122f1da5c6b51906aed9c3f1dd8c43e7179ae8
Calle <calle.samuelsson.14@gmail.com>
2021-09-27 16:18:57 +0200

    FAQ sidan språk och ikoner

--- android-app/app/src/main/java/com/example/covid_tracker/Faq.java
+++ android-app/app/src/main/java/com/example/covid_tracker/Faq.java
@@ -1,6 +1,10 @@
 package com.example.covid_tracker;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +13,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Faq extends Fragment {
 
     private View view;
+    private boolean language = false;
     RecyclerView recyclerView;
     List<FAQ_block> list;
 
@@ -25,13 +33,70 @@ public class Faq extends Fragment {
         // tex    button = (Button) view.findViewById(R.id.button); viktigt att det står view. för att komma åt element i fragmented
 
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView_faqis);
+        FloatingActionButton buttonFAQ = view.findViewById(R.id.buttonFAQ);
+
+        setAppLocate("en");
 
 
         initQuestions();
         setRecyclerView();
 
+
+        buttonFAQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                language = !language;
+
+                for(int i=0;i<list.size();i++){
+
+                    list.remove(0);
+
+                }
+
+                if(language) {
+                    FloatingActionButton buttonFAQ = view.findViewById(R.id.buttonFAQ);
+                    buttonFAQ.setImageResource(R.drawable.flaggb_foreground);
+                    setAppLocate("sv");
+
+                }
+                else{
+
+                    FloatingActionButton buttonFAQ = view.findViewById(R.id.buttonFAQ);
+                    buttonFAQ.setImageResource(R.drawable.flagswe_foreground);
+
+                    setAppLocate("en");
+                }
+
+                initQuestions();
+                setRecyclerView();
+
+                System.out.println("HEJ DU KLICKA PÅ SPRÅK");
+            }
+
+        });
         //botten
         return view;
+    }
+
+
+    public void setAppLocate(String language){
+
+
+        Resources re = getResources();
+        DisplayMetrics dm = re.getDisplayMetrics();
+        Configuration config = re.getConfiguration();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1){
+            config.setLocale(new Locale(language.toLowerCase()));
+        }
+        else{
+
+            config.locale = new Locale(language.toLowerCase());
+
+        }
+
+
+        re.updateConfiguration(config, dm);
     }
 
     private void setRecyclerView() {
@@ -47,9 +112,31 @@ public class Faq extends Fragment {
 
         list = new ArrayList<>();
 
-        list.add(new FAQ_block("Calle", "balle"));
-        list.add(new FAQ_block("Har du ätit idag?", "ja det har jag"));
-        list.add(new FAQ_block("Är calle bög?", "ja"));
+        if(language) {
+            list.add(new FAQ_block(getString(R.string.Fråga1), "In Karlstad"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "SVENSKA"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "In Karlstad"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "SVENSKA"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "In Karlstad"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "SVENSKA"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "In Karlstad"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "SVENSKA"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "In Karlstad"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "SVENSKA"));
+        }
+        else{
+
+            list.add(new FAQ_block(getString(R.string.Fråga1), "In Karlstad"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "SVENSKA"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "In Karlstad"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "SVENSKA"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "In Karlstad"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "SVENSKA"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "In Karlstad"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "SVENSKA"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "In Karlstad"));
+            list.add(new FAQ_block(getString(R.string.Fråga1), "SVENSKA"));
+        }
 
 
     }
