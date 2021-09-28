a5069380d6fbdb6dadb39f2cd015081e53feb5b7
diego <diego.gryth@hotmail.com>
2021-09-28 17:36:20 +0200

    admin dashboard design done

---
+++ android-app/app/src/main/java/com/example/covid_tracker/UppcommingApointments.java
@@ -0,0 +1,14 @@
+package com.example.covid_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UppcommingApointments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uppcomming_apointments);
+    }
+}
