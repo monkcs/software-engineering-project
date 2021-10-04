c1c0b66d0e448c66339cf506bfa1ceff2cac636d
AmandaJohnsson <amanda-johnsson@hotmail.com>
2021-10-04 14:45:33 +0200

    implemented server for todays appointments

---
+++ today_appointments.php
@@ -0,0 +1,30 @@
+<?
+
+require 'authenticate.php'
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    
+    $statement = $connection->prepare("SELECT firstname, surname, telephone from person inner join appointment
+                                    on person.account = appointment.account
+                                    inner join available
+                                    on appointment.available = available.id
+                                    and available.provider = $identity
+                                    and cast(datetime AS Date) = cast(getdate() as Date)");
+
+    $statement->execute();
+    $result = $statement->get_result();
+    $statement->close();
+
+    if ($result->num_rows == 0) {
+        http_response_code(400);
+        echo "No appointments booked today\n";
+    } else {
+        echo json_encode($result->fetch_all(MYSQLI_ASSOC));
+        echo "\n";
+    }
+}else {
+    http_response_code(405);
+    echo "Send request using HTTP get\n";
+}
+
+?>
