0d0aedc4f799b729450e227cd12bd6cdf280e83e
diego <diego.gryth@hotmail.com>
2021-10-06 12:15:51 +0200

    scroller added in xml, start in java code

--- android-app/app/src/main/java/com/example/covid_tracker/PlanAndShedVaccs.java
+++ android-app/app/src/main/java/com/example/covid_tracker/PlanAndShedVaccs.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.covid_tracker.Fragments.BookingStep1Fragment;
+
+import java.util.ArrayList;
+
 public class PlanAndShedVaccs extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_and_shed_vaccs);
+
+        //private ArrayList<BookingStep1Fragment.Provider> clinics = new ArrayList<>();
     }
 }
