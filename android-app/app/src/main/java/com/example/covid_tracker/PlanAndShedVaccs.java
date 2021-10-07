198cd730dacd3a0439b36d5871ba91568c64a2ee
diego <diego.gryth@hotmail.com>
2021-10-07 16:11:36 +0200

    Age Spinner selection without return value implemented

--- android-app/app/src/main/java/com/example/covid_tracker/PlanAndShedVaccs.java
+++ android-app/app/src/main/java/com/example/covid_tracker/PlanAndShedVaccs.java
@@ -4,21 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Locale;
 
-public class PlanAndShedVaccs extends AppCompatActivity {
+public class PlanAndShedVaccs extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner ageSpinner, dateSpinner;
     private Calendar calendar;
     private SimpleDateFormat sdf;
     private String strDate;
-    private ArrayList<String> ageSpan, dateList;
+    private ArrayList<String> ageList, dateList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +30,28 @@ public class PlanAndShedVaccs extends AppCompatActivity {
         ageSpinner = (Spinner) findViewById(R.id.age_spinner);
         dateSpinner = (Spinner) findViewById(R.id.start_date_spinner);
 
-        ageSpan = new ArrayList<>();
-        ageSpan.add("Age");
-        ageSpan.add("65+");
-        ageSpan.add("55+");
-        ageSpan.add("45+");
-        ageSpan.add("18+");
+        ageList = new ArrayList<>();
+        ageList.add("Age");
+        ageList.add("65+");
+        ageList.add("55+");
+        ageList.add("45+");
+        ageList.add("18+");
 
+        ArrayAdapter<String> ageAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_spinner_item,
+                        ageList); //selected item will look like a spinner set from XML
+        ageAdapter.setDropDownViewResource(android.R.layout
+                .simple_spinner_dropdown_item);
+        ageSpinner.setAdapter(ageAdapter);
+        ageSpinner.setOnItemSelectedListener(this);
+
+
+        /*
         dateList = new ArrayList<>();
         dateList.add("Datum: yyuy-mm-dd");
 
         //skapar ett kalender object som formateras till en string
-        /*calendar = Calendar.getInstance();
+        calendar = Calendar.getInstance();
         Log.i("testa log 1", "Current time:" + calendar);
         sdf = new SimpleDateFormat("yyyy-MM-dd");
         strDate = sdf.format(calendar);
@@ -53,5 +66,15 @@ public class PlanAndShedVaccs extends AppCompatActivity {
         * sedan skall 15 min tidsintervall laggas till frn sag kl 06:00 till 20:00*/
 
 
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+    //what should go here?
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+    //what should go here??
     }
 }
