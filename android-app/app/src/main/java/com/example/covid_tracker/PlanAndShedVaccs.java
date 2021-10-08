aa1512e68eb160d4bbf07aa059345439d2c1ca45
diego <diego.gryth@hotmail.com>
2021-10-08 10:42:43 +0200

    start Date Added to spinner

--- android-app/app/src/main/java/com/example/covid_tracker/PlanAndShedVaccs.java
+++ android-app/app/src/main/java/com/example/covid_tracker/PlanAndShedVaccs.java
@@ -9,41 +9,55 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import com.google.android.material.navigation.NavigationBarView;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 public class PlanAndShedVaccs extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    Spinner ageSpinner, dateSpinner;
-    private Calendar calendar;
+    Spinner ageSpinner, timeSpinner;
+    private Calendar calendar, start,end;
+    private Date today;
     private SimpleDateFormat sdf;
     private String strDate;
-    private ArrayList<String> ageList, dateList;
+    private ArrayList<String> ageList;
+    private ArrayList<Date> timeList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_and_shed_vaccs);
+
+        //-------------------------------------------------
         ageSpinner = (Spinner) findViewById(R.id.age_spinner);
-        dateSpinner = (Spinner) findViewById(R.id.start_date_spinner);
-
         ageList = new ArrayList<>();
-        ageList.add("Age");
-        ageList.add("65+");
-        ageList.add("55+");
-        ageList.add("45+");
-        ageList.add("18+");
 
+        setAgeList();
         ArrayAdapter<String> ageAdapter = new ArrayAdapter<String>
                 (this, android.R.layout.simple_spinner_item,
                         ageList); //selected item will look like a spinner set from XML
-        ageAdapter.setDropDownViewResource(android.R.layout
-                .simple_spinner_dropdown_item);
+        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         ageSpinner.setAdapter(ageAdapter);
         ageSpinner.setOnItemSelectedListener(this);
+
+        //---------------------------------------------------------
+
+        timeSpinner = (Spinner) findViewById(R.id.start_date_spinner);
+        timeList = new ArrayList<>();
+
+        today = Calendar.getInstance().getTime();
+
+        timeList.add(today);
+        ArrayAdapter<Date> timeAdapter = new ArrayAdapter<Date>
+                (this, android.R.layout.simple_spinner_item, timeList);
+        //selected item will look like a spinner set from XML
+
+        timeAdapter.setDropDownViewResource(android.R.layout
+                .simple_spinner_dropdown_item);
+        timeSpinner.setAdapter(timeAdapter);
+        timeSpinner.setOnItemSelectedListener(this);
 
 
         /*
@@ -66,6 +80,23 @@ public class PlanAndShedVaccs extends AppCompatActivity implements AdapterView.O
         * sedan skall 15 min tidsintervall laggas till frn sag kl 06:00 till 20:00*/
 
 
+    }
+
+    public  void setAgeList(){
+        ageList.add("Age");
+        ageList.add("65+");
+        ageList.add("55+");
+        ageList.add("45+");
+        ageList.add("18+");
+        ageList.add("0+");
+    }
+
+    public void setTimeList(Date startDate){
+        // int minutes, Calender startDate, Calender endDate
+        //Produceras tidsintervaller mellan two datum,
+        // skall ocksa timmar laggas in? -> lar dig Calender Classen forst..
+        //aList.add("Date");
+        timeList.add(startDate);
     }
 
     @Override
