fa951827b9f1ea8fa369d46bc561cbe81fae2208
monkcs <charlie.habolin@citroner.blog>
2021-10-09 12:17:43 +0200

    Implement current date lock

--- server/user/appointment/available.php
+++ server/user/appointment/available.php
@@ -13,7 +13,21 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         exit;
     }
 
-    $statement = $connection->prepare("SELECT * FROM available where NOT EXISTS (SELECT * FROM appointment WHERE appointment.available = available.id) AND available.provider = ?");
+    /* SELECT * 
+FROM available
+WHERE
+available.datetime > '2021-10-07'
+
+SELECT CURRENT_DATE;*/
+
+    /*
+    SELECT *  FROM prioritizations
+    WHERE prioritizations.not_younger <= 23
+    ORDER BY not_younger DESC
+    LIMIT 1
+*/
+
+    $statement = $connection->prepare("SELECT * FROM available WHERE NOT EXISTS (SELECT * FROM appointment WHERE appointment.available = available.id) AND available.provider = ? AND available.datetime > CURRENT_DATE");
     $statement->bind_param("i", $provider);
     $statement->execute();
     $result = $statement->get_result();
