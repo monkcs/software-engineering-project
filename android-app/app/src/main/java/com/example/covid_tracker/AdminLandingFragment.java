2b93d070103feec1b27f2826fb12578d286b454e
axelalvin <axelalvin1996@gmail.com>
2021-10-11 15:57:23 +0200

    implemented fragments into admin side

---
+++ android-app/app/src/main/java/com/example/covid_tracker/AdminLandingFragment.java
@@ -0,0 +1,67 @@
+package com.example.covid_tracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AdminLandingFragment extends Fragment {
+
+    private RelativeLayout rl1, rl2, rl3;
+    private View view;
+    //private TextView userCount = findViewById(R.id.adminUserCount);
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_admin_landing, container, false);
+        //setContentView(R.layout.fragment_admin_dashboard);
+        rl1= (RelativeLayout) view.findViewById(R.id.Rellay1);
+        /*rl1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uppcApoint();
+            }
+        });*/
+
+        rl2 = (RelativeLayout) view.findViewById(R.id.Rellay2);
+        rl2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                planAndShedVacc();
+            }
+        });
+
+        rl3 = (RelativeLayout) view.findViewById(R.id.Rellay3);
+        /*rl3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inboxAdmin();
+            }
+        });*/
+
+        return view;
+    }
+
+    public void inboxAdmin(){
+        Intent intent = new Intent(getActivity(), inboxAdmin.class);
+        startActivity(intent);
+    }
+
+    public void uppcApoint(){
+        Intent intent = new Intent(getActivity(), UpcomingAppointments.class);
+        startActivity(intent);
+    }
+
+    public void planAndShedVacc(){
+        Intent intent = new Intent(getActivity(), PlanAndShedVaccs.class);
+        startActivity(intent);
+    }
+
+
+}
