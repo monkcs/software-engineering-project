302879221d4bf285bfe2f597e106d76dfe0d777a
Calle <calle.samuelsson.14@gmail.com>
2021-10-14 12:57:46 +0200

    fixat för och efternamn// Calle

--- android-app/app/src/main/java/com/example/covid_tracker/Admin_block.java
+++ android-app/app/src/main/java/com/example/covid_tracker/Admin_block.java
@@ -8,13 +8,14 @@ public class Admin_block {
 
     private int Appointment;
     public int ID, available;
-    public String personen, svaret, telenmr, datumTid;
+    public String personen, personen2, svaret, telenmr, datumTid;
     private Boolean expandable;
 
 
     public Admin_block(JSONObject serialized, String questions) throws JSONException {
 
         this.personen = serialized.getString("firstname");
+        this.personen2 = serialized.getString("surname");
         this.telenmr = serialized.getString("telephone");
         this.datumTid = serialized.getString("datetime");
         this.ID = serialized.getInt("account");
@@ -75,11 +76,38 @@ public class Admin_block {
         this.expandable = expandable;
     }
 
+    public int getAppointment() {
+        return Appointment;
+    }
+
+    public void setAppointment(int appointment) {
+        Appointment = appointment;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    public String getPersonen2() {
+        return personen2;
+    }
+
+    public void setPersonen2(String personen2) {
+        this.personen2 = personen2;
+    }
+
     @Override
     public String toString() {
         return "Admin_block{" +
-                "ID=" + ID +
+                "Appointment=" + Appointment +
+                ", ID=" + ID +
+                ", available=" + available +
                 ", personen='" + personen + '\'' +
+                ", personen2='" + personen2 + '\'' +
                 ", svaret='" + svaret + '\'' +
                 ", telenmr='" + telenmr + '\'' +
                 ", datumTid='" + datumTid + '\'' +
