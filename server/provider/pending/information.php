b6f6f12ab3fb8ad46b07aa02a48ba046da3cf474
monkcs <charlie.habolin@citroner.blog>
2021-10-14 11:44:57 +0200

    Fix pending inbox error

--- server/provider/pending/information.php
+++ server/provider/pending/information.php
@@ -4,15 +4,8 @@ require 'authenticate.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-        $appointment = $_POST["appointment"];
-        if ($appointment == "") {
-                http_response_code(400);
-                echo "No appointment specified\n";
-                exit;
-        }
-
-        $statement = $connection->prepare("SELECT text FROM pending INNER JOIN question ON pending.question = question.id WHERE pending.appointment = ? AND pending.provider = ?");
-        $statement->bind_param("ii", $appointment, $identity);
+        $statement = $connection->prepare("SELECT pending.appointment, text FROM pending INNER JOIN question ON pending.question = question.id WHERE  pending.provider = ?");
+        $statement->bind_param("i", $identity);
         $statement->execute();
         $result = $statement->get_result();
         $statement->close();
